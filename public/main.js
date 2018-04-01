5531e8ead0e9c80aacd3f4a954336b848e0cc352
AaronStPete <AaronStPete@users.noreply.github.com>
2018-03-31 21:37:47 -0400

    pulls 5 random categoryIDs

--- public/main.js
+++ public/main.js
@@ -1,5 +1,54 @@
-const main = () => {
-  document.querySelector('h1').textContent += '?'
-}
+const BASE_URL = "http://jservice.io/"
+let categoryArr = []
+const CAT_URL = "http://jservice.io/api/clues?category="
 
-document.addEventListener('DOMContentLoaded', main)
+angular
+  .module("mainApp", [])
+  .controller("mainController", ["$scope", "$http",($scope, $http) => {
+    //////These 5 calls are to randomly pull 5 categories
+    $http({
+      method: "GET",
+      url: BASE_URL + "api/random/",
+    }).then(response => {
+      let categoryID = response.data[0].category_id
+      categoryArr.push(categoryID)
+    })
+
+    $http({
+      method: "GET",
+      url: BASE_URL + "api/random/",
+    }).then(response => {
+      let categoryID = response.data[0].category_id
+      categoryArr.push(categoryID)
+    })
+    $http({
+      method: "GET",
+      url: BASE_URL + "api/random/",
+    }).then(response => {
+      let categoryID = response.data[0].category_id
+      categoryArr.push(categoryID)
+    })
+    $http({
+      method: "GET",
+      url: BASE_URL + "api/random/",
+    }).then(response => {
+      let categoryID = response.data[0].category_id
+      categoryArr.push(categoryID)
+    })
+    $http({
+      method: "GET",
+      url: BASE_URL + "api/random/",
+    }).then(response => {
+      let categoryID = response.data[0].category_id
+      categoryArr.push(categoryID)
+      console.log("Your Category IDs are: ", categoryArr);
+    })
+    ///////This next portion is to pull 6 random questions from each category
+    $http({
+      method: "GET",
+      url: CAT_URL + (categoryArr[0]),
+    }).then(response => {
+      console.log(categoryArr[0])
+      console.log(response)
+    })
+  }])
